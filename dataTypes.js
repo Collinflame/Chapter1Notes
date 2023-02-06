269b36cb19bc2f52eb0ce502c65e59b62a3ffd95
3siy <3siy@fresnou.org>
2023-02-06 13:25:37 -0800

    initial control

---
+++ dataTypes.js
@@ -0,0 +1,37 @@
+console.log(8);
+
+//This is a comment
+
+//Numbers
+ //Integers
+console.log(-3);
+console.log(0);
+console.log(9);
+
+//Floating Point Numbers (Floats)
+console.log(3.45);
+console.log(-0.03);
+console.log(134.);
+
+//Strings
+ //Characters, Letters, Symbols, Spaces
+console.log("Hi Java");
+console.log('JS');
+console.log(`Backticks By 1`);
+
+//Arithmetic Operators (Math)
+console.log(14-2);
+console.log(6+3);
+console.log(3*9); //shift+8 fives *
+console.log(31/4);
+console.log(30%4); //% is called MOD
+                   // it Means Remainder After Dividing
+
+// Concatenation means to combine strings into a single string
+console.log("My name "+"is Spider-Man.");
+console.log("My"+ " "+ "age"+ " "+ "is "+ 26);
+console.log("Your age is\n17");
+
+// Template literal – plug in some calculated value, then build the string on the fly
+ //use backticks `` next to the 1
+console.log(`200 divided by 8 equals ${200/8}`)
